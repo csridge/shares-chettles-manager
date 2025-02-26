b7604a69b03bd3ef4576483c28128e945b78a0da
John Ridge <johnridgevn@gmail.com>
2025-02-26 15:18:33 +0700

    Login page

--- login.py
+++ login.py
@@ -1,22 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import hashlib
 from flask_cors import CORS
-from inventory import default_inventory
+from users import users
 
 app = Flask(__name__)
 CORS(app)
-
-users = {
-    "jsaidoru": {"password": hashlib.sha256("pyaidoru".encode()).hexdigest()},
-    "availablegreen": {"password": hashlib.sha256("availablegreen".encode()).hexdigest()},
-    "bozo0069": {"password": hashlib.sha256("gothamchesssucks".encode()).hexdigest()},
-    "erix_senpai": {"password": hashlib.sha256("ihategenshin".encode()).hexdigest()},
-    "kan1234567891011121314": {"password": hashlib.sha256("kanlol".encode()).hexdigest()},
-    "ghoda": {"password": hashlib.sha256("bulletenjoyer".encode()).hexdigest()},
-    ".wigeon.": {"password": hashlib.sha256("kyobir".encode()).hexdigest()},
-    "i_am_not_kk": {"password": hashlib.sha256("iamkk".encode()).hexdigest()},
-    "stedwesd": {"password": hashlib.sha256("car95".encode()).hexdigest()}
-}
 
 @app.route('/')
 def home():
