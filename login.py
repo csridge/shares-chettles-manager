783d6659c65765e770097a1f49ea8aeaf4175fef
John Ridge <johnridgevn@gmail.com>
2025-02-26 15:09:07 +0700

    Initial Flask API commit

--- login.py
+++ login.py
@@ -6,10 +6,16 @@ from inventory import default_inventory
 app = Flask(__name__)
 CORS(app)
 
-# Example users
 users = {
     "jsaidoru": {"password": hashlib.sha256("pyaidoru".encode()).hexdigest()},
-    "availablegreen": {"password": hashlib.sha256("availablegreen".encode()).hexdigest()}
+    "availablegreen": {"password": hashlib.sha256("availablegreen".encode()).hexdigest()},
+    "bozo0069": {"password": hashlib.sha256("gothamchesssucks".encode()).hexdigest()},
+    "erix_senpai": {"password": hashlib.sha256("ihategenshin".encode()).hexdigest()},
+    "kan1234567891011121314": {"password": hashlib.sha256("kanlol".encode()).hexdigest()},
+    "ghoda": {"password": hashlib.sha256("bulletenjoyer".encode()).hexdigest()},
+    ".wigeon.": {"password": hashlib.sha256("kyobir".encode()).hexdigest()},
+    "i_am_not_kk": {"password": hashlib.sha256("iamkk".encode()).hexdigest()},
+    "stedwesd": {"password": hashlib.sha256("car95".encode()).hexdigest()}
 }
 
 @app.route('/')
@@ -28,4 +34,4 @@ def login():
     return jsonify({"success": False, "message": "Invalid username or password"}), 401
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run(host='0.0.0.0', port=5000)
